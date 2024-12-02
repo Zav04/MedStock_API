32c3f0fe0dc8237ca9e4d5d77909ebe5d06dd8ec
Bruno Oliveira <bruno.bx04@gmail.com>
2024-12-02 01:45:12 +0000

    V1.01

--- main.py
+++ main.py
@@ -9,9 +9,11 @@ from REQUESTS_MedStock.ITENS.GET_Itens import router as GET_itens_router
 from REQUESTS_MedStock.REQUERIMENTO.GET_REQUERIMENTO import router as GET_requerimento_router
 from REQUESTS_MedStock.LOGIN.GET_Login import router as GET_login_router
 from REQUESTS_MedStock.SETORES.GET_Setor import router as GET_setor_router
+from REQUESTS_MedStock.REQUERIMENTO.POST_REQUERIMENTO import router as POST_requerimento_router
 from REQUEST_MedReader.LOGIN.POST_Login import router as POST_login_router_MedReader
 from REQUEST_MedReader.REQUERIMENTO.GET_REQUERIMENTO import router as GET_requerimento_router_MedReader
 from REQUEST_MedReader.REQUERIMENTO.PUT_REQUERIMENTO import router as PUT_requerimento_router_MedReader
+from REQUESTS_MedStock.REQUERIMENTO.PUT_REQUERIMENTO import router as PUT_requerimento_router
 api = FastAPI()
 
 api.add_middleware(
@@ -28,8 +30,12 @@ api.include_router(POST_create_user_router)
 api.include_router(GET_roles_router)
 api.include_router(GET_itens_router)
 api.include_router(GET_requerimento_router)
+api.include_router(POST_requerimento_router)
 api.include_router(GET_login_router)
 api.include_router(GET_setor_router)
+api.include_router(PUT_requerimento_router)
+
+
 api.include_router(POST_login_router_MedReader)
 api.include_router(GET_requerimento_router_MedReader)
 api.include_router(PUT_requerimento_router_MedReader)
